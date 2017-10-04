1dae5fab631f7e017e9c373d3b055150918aef62
Nickolai Belakovski <nbelakovski@gmail.com>
2017-10-03 18:11:40 -0700

    More messing with visualization. Looks OK now

--- visualization.py
+++ visualization.py
@@ -33,32 +33,39 @@ for scene in data:
         )
     ]
 
+    camera = dict(
+            up=dict(x=1, y=0, z=0),
+            center=dict(x=0, y=0, z=0),
+            eye=dict(x=0, y=-2, z=1)
+    )
     layout = go.Layout(
         title='Elevation near Glacier Peak, WA',
-        autosize=False,
-        width=1200,
-        height=800,
-        margin=dict(
-                l=65,
-                r=50,
-                b=65,
-                t=90
-        ),
+        autosize=True,
+        # width=1200,
+        # height=800,
+        # margin=dict(
+        #         l=65,
+        #         r=50,
+        #         b=65,
+        #         t=90
+        # ),
         images=[dict(
                 source='https://raw.githubusercontent.com/nbelakovski/topography_neural_net/master/WA_GlacierPeak_2014_000070.jpg',#'https://raw.githubusercontent.com/cldougl/plot_images/add_r_img/vox.png', #'https://earthexplorer.usgs.gov/browse/lidar/WA/2014/WA_GlacierPeak_2014/WA_GlacierPeak_2014_000070.jpg', #scene[0]['browseUrl'],
                 xref="paper", yref="paper",
-                x=-0.07, y=0.88,
+                x=-0.07, y=0.83,
                 sizing="stretch",
-                sizex=0.6, sizey=0.8, layer="below",
+                sizex=0.4, sizey=0.7, layer="below",
                 xanchor = 'left', yanchor='top'
         )],
     )
     print(scene[0]['browseUrl'])
 
     # fig = go.Figure(data=plot_data, layout=layout)
-    fig = pt.make_subplots(1,2, specs=[[{'is_3d':False}, {'is_3d':True}]])
+    fig = pt.make_subplots(1,3, specs=[[{'is_3d':True}, {'is_3d':True}, {'is_3d':True}]])
     fig.append_trace(plot_data[0], 1, 2)
     fig['layout'].update(layout)
+    fig['layout'].update(scene2=dict(camera=camera))
+
     a = po.plot(fig)
     print(a)
 
