4163fbe28f0b02a9828de225c34867649a8f0524
Nickolai Belakovski <nbelakovski@gmail.com>
2017-10-07 17:31:25 -0700

    Changes to visualizer just to make the current image something that I could show people. I don't usually do progress reports like this in a project, but this feels good :)

--- visualization.py
+++ visualization.py
@@ -42,7 +42,7 @@ for i, scene in enumerate(data):
 
     # Grab the image associated with the lidar dataset to display next to the 3d surface plot. For now, images will be
     # stored in my github
-    image_filename = scene[0]['browseUrl'].split('/')[-1]
+    image_filename = 'cropped.jp2'
     image_source = 'https://raw.githubusercontent.com/nbelakovski/topography_neural_net/master/data/' + str(i) + '/' + \
                    image_filename
     layout = go.Layout(
@@ -60,6 +60,19 @@ for i, scene in enumerate(data):
                 xanchor='left', yanchor='top'
             )
         ],
+        annotations=[
+            dict(
+                x=.975,
+                y=.5,
+                xref='paper',
+                yref='paper',
+                text='TODO:' + '<br>' + 'Neural Net Topography' + '<br>' + 'Output Here',
+                showarrow=False,
+                font=dict(
+                        size=36,
+                )
+            )
+        ],
     )
 
     # Set up subplots. One for the lidar image, next for the original data, next for the machine learned data
@@ -71,3 +84,4 @@ for i, scene in enumerate(data):
 
     # PLOT!
     a = po.plot(fig)
+    break
