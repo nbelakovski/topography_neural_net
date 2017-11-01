b30ee129315e4567c77997c087a789029818b7da
Nickolai Belakovski <nbelakovski@gmail.com>
2017-11-01 01:57:23 -0700

    Finished code for 0-interpolation. Looks good from the few samples I've looked at.

--- data/interpolate_zeros.cpp
+++ data/interpolate_zeros.cpp
@@ -7,16 +7,60 @@ namespace matrix_ops
 
 void interpolate_zeros(pybind11::array_t<int> & matrix)
 {
-  /*pybind11::buffer_info b = matrix.request();
-  void * const ptr = b.ptr;  // make it const so that we have the initial location always stored
-  int * data = static_cast<int*>(ptr);
-  *data = 56;
-  std::cout << *(static_cast<const int*>(ptr) + 4) << std::endl;*/
-  std::cout << *(static_cast<int*>(matrix.mutable_data(0,0))) << std::endl;
-  int * data = static_cast<int*>(matrix.mutable_data(0,1));
-  *data = 78;
-  std::cout << matrix.shape()[0] << std::endl;
-  std::cout << matrix.shape()[1] << std::endl;
+  int rows = matrix.shape()[0];
+  int cols = matrix.shape()[1];
+  for (int row = 0; row < rows; ++row)
+  {
+    int start_col = -1;
+    int end_col = -1;
+    for (int col = 0; col < cols; ++col)
+    {
+      const int * data = static_cast<const int*>(matrix.data(row, col));
+      if (start_col == -1 && *data == 0)
+      {
+        start_col = col;
+      }
+      if (start_col != -1 && end_col == -1 && *data != 0)
+      {
+        end_col = col; // this might not get reached if line ends with a 0
+      }
+
+      // if end_col is -1, it could mean a 0 at the end, meaning we should
+      // also check to see if we are at the end
+      if (start_col != -1 && ((end_col != -1) || col == cols - 1))
+      {
+        int start_val = 0;
+        int end_val = 0;
+        if (start_col == 0 && end_col == -1)
+        {
+          std::cerr << "Entire row is blank. Fucking off..." << std::endl;
+          return;
+        }
+        else if (start_col == 0)
+        {
+          start_val = end_val = *static_cast<const int*>(matrix.data(row, end_col));
+        }
+        else if (end_col == -1)
+        {
+          end_col = cols;
+          start_val = end_val = *static_cast<const int*>(matrix.data(row, start_col - 1));
+        }
+        else
+        {
+          start_val = *static_cast<const int*>(matrix.data(row, start_col - 1));
+          end_val   = *static_cast<const int*>(matrix.data(row, end_col));
+        }
+        float slope = float(end_val - start_val) / float(end_col - start_col + 1);
+        int intercept = start_val; // even in the case of a line starting with a 0, this is ok, since start_val == end_val in that case
+        for (int i = start_col; i < end_col; ++i)
+        {
+          int * data = static_cast<int*>(matrix.mutable_data(row, i));
+          *data = slope * (i - start_col + 1) + intercept;
+        }
+        start_col = end_col = -1;
+      }
+    }
+  }
 }
 
 }
