7da811ba51de428f5e5c4fa84429641f72575ea2
Nickolai Belakovski <nbelakovski@gmail.com>
2017-12-21 23:52:42 -0500

    Removed index.html, renamed html page for generated site

--- evaluate.sh
+++ evaluate.sh
@@ -6,5 +6,5 @@ DATA=$2
 # This will create a file called test.pickle, which will be autoimported by visualization.py
 python3 inference.py $IMAGE
 python3 visualization.py $DATA
-mv temp-plot.html docs/temp-plot.html
+mv temp-plot.html docs/plots.html
 python3 -c "from pgmagick import Image; Image('$IMAGE').write('docs/cropped.jpg')"
